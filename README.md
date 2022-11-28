223c067c6274aa9a37863079f24a913a0d8979a8
LudBjork <ludvikb@hotmail.no>
2022-11-28 13:49:48 +0100

    Added working rock paper scissors, updated README.md

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Website_Development
 Trying to make a webpage based off my HTML knowledge (thus far)
 # ChangeLog
-*10.11.2022:* Added About Me page and added some content to it (hobbies, my academic background)
+*28.11.2022*: Added working rock paper scissors after having gone through W3 Schools basic javascript tutorial.
+*18.11.2022*: Added Rock/Paper/Scissors outline. Will replace Projects page for the rock/paper/scissors one instead, as it is my current main project I'd like to show off.
+*10.11.2022*: Added About Me page and added some content to it (hobbies, my academic background)
--- rps.js
+++ rps.js
@@ -1,23 +1,15 @@
-function game(id) {
-    /* 
-        Temp. pseudocode
-        Given btn-press r / p / s 
-        Code game rules (r>s, s>p, p>r)
-        Implement Reset button for the game
-        Use prompt to declare winner and current score. 
-    */
-    const player1 = id;
-    var player2 = Math.floor(Math.random() * 3);
-    const translate = new Map([
-        [0, "rock"],
-        [1, "paper"],
-        [2, "scissors"],
-    ]);
-    player2 = translate.get(player2);
-    alert(player1, player2);
-    var win = document.getElementById("win");
-    var loss = document.getElementById("loss");
-    var tie = document.getElementById("tie");
+function game(player1) {
+    let y = Math.floor(Math.random() * 3);
+    const translate = ["rock", "paper", "scissors"];
+    let player2 = translate[y];
+    const win = document.getElementById("win");
+    const loss = document.getElementById("loss");
+    const tie = document.getElementById("tie");
+    translate.splice(y, 1);
+    for (let id of translate) {
+        document.getElementById(id).style.display = "none"; // hide the other options
+    }
+
     if (player1 == player2) {
         tie.style.display = "block";
     } else if ((player1 == "rock") & (player2 == "scissors")) {
@@ -29,4 +21,8 @@ function game(id) {
     } else {
         loss.style.display = "block";
     }
+    document.getElementById("comp_choice").innerHTML += player2;
+    document.getElementById("comp_choice").style.display = "block";
+    document.getElementById("resetter").style.display = "block";
+    document.getElementById(player2).onclick = "";
 }
